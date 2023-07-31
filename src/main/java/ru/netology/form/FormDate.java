72104c5689cc8e3f7bf4941faf5796dd3da18cf7
VladimirTikhonov <Vovantikhonov@inbox.ru>
2023-07-31 23:56:06 +0300

    Initial Commit

---
+++ src/main/java/ru/netology/form/FormDate.java
@@ -0,0 +1,9 @@
+package ru.netology.form;
+
+public class FormDate {
+    public int day;
+    public int month;
+    public int year;
+
+}
+
---
+++ src/main/java/ru/netology/form/Post.java
@@ -0,0 +1,20 @@
+package ru.netology.form;
+
+public class Post {
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public boolean subscription;
+    public FormDate birthday;
+
+}
+
+
+//"name": "Иван",
+// "passport": "4444 № 44444444",
+//"patronymic": "Иванович",
+//"phone": "+7 (999)-999-99-99",
+//"surname": "Иванов",
+//"subscription": true
